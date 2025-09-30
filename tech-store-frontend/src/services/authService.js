b1ed3dcb25bbdb49a9306e87d74738af365b7f20
Tecnobotoutlet <tecnobotoutlet@gmail.com>
2025-09-30 09:07:25 -0500

    Update authService.js

--- tech-store-frontend/src/services/authService.js
+++ tech-store-frontend/src/services/authService.js
@@ -1,9 +1,7 @@
 // src/services/authService.js - Con Supabase Auth Real
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 
 export const authService = {
   // Registro
