be48d540af7ad1983bb981e6cc19635ac75cbba6
Tecnobotoutlet <tecnobotoutlet@gmail.com>
2025-09-30 08:47:53 -0500

    Update userService.js

--- tech-store-frontend/src/services/userService.js
+++ tech-store-frontend/src/services/userService.js
@@ -3,7 +3,7 @@ import { supabase } from '../supabaseClient';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 
 export const userService = {
   // Obtener todos los usuarios con estadísticas
