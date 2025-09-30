1ca13d12f22d82f356ecec4af10c0056f15e557a
Tecnobotoutlet <tecnobotoutlet@gmail.com>
2025-09-30 08:36:29 -0500

    Add shared Supabase client

---
+++ tech-store-frontend/src/supabaseClient.js
@@ -0,0 +1,8 @@
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+// Crear UNA SOLA instancia
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
