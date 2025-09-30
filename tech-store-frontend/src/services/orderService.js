b1eeafa0ca3aa87fecb15689257a1e7519d7bb4b
Tecnobotoutlet <tecnobotoutlet@gmail.com>
2025-09-30 08:47:26 -0500

    Update orderService.js

--- tech-store-frontend/src/services/orderService.js
+++ tech-store-frontend/src/services/orderService.js
@@ -3,7 +3,7 @@ import { supabase } from '../supabaseClient';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 
 export const orderService = {
   // Obtener todos los pedidos con items
