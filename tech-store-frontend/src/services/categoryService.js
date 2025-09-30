134a5cc64558056f812227fef042a335616e6645
Tecnobotoutlet <tecnobotoutlet@gmail.com>
2025-09-30 09:07:46 -0500

    Update categoryService.js

--- tech-store-frontend/src/services/categoryService.js
+++ tech-store-frontend/src/services/categoryService.js
@@ -2,8 +2,7 @@
 
 import { supabase } from '../supabaseClient';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
 
 
 export const categoryService = {
